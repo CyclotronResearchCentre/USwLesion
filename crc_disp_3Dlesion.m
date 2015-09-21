e9679697727a39fda1cbc48a79e17fba93fd9656
Christophe Phillips <c.phillips@ulg.ac.be>
2015-09-21 11:54:34 +0200

    Improved display function
    Avoid recalculating a mesh every time the plot is updated by creating
    the whole plot but making some bits transparent!
    Now it is sufficient to just pass a parameter structure to update the
    existing plot, i.e. much lighter than passing the volume matrix and
    re-estimating the meshing.

--- crc_disp_3Dlesion.m
+++ crc_disp_3Dlesion.m
@@ -1,13 +1,14 @@
-function [p_handles, dat_out] = crc_disp_3Dlesion(Ps,Pl,param,other_in)
-
-% FUNCTION [p_handles, dat_out] = crc_disp_3Dlesion(Ps,Pl,param,other_in)
+% function [p_handles, dat_out] = crc_disp_3Dlesion(Ps,Pl,param,other_in)
+function [param_out] = crc_disp_3Dlesion(varargin)
+% FUNCTION param_out = crc_disp_3Dlesion(Ps,Pl,param)
+% or
+% FUNCTION crc_disp_3Dlesion(param)
 %
-% Plot a 3D visualisation of lesion/blob over a cut structural image. 
-% It is also possible to update an exising plot by passign the data, 
-% parameters and some extra input. This is particularly useful to create 
-% frames for a movie.
+% Plot a 3D visualisation of lesion/blob over a cut structural image.
+% It is also possible to update an exising plot by passign a set of 
+% parameters. This is particularly useful to create frames for a movie.
 %
-% INPUT
+% INPUT for a new plot
 % - Ps : filename of structural image (or structural image 3D matrix for
 %        plot update)
 % - Pl : filename of structural image (not needed for plot update)
@@ -20,32 +21,39 @@ function [p_handles, dat_out] = crc_disp_3Dlesion(Ps,Pl,param,other_in)
 %   .col_isosurf : colour for structural volume isosurface, def. [1 .6 .6]
 %   .col_blobs : colour for blobs, def. 'green'
 %   .rotate3D : allow manual 3D rotation in plot, def. false
-% - other_in = structure with the following possible fields (optional)
-%   .xyz_mm : coordinates in mm, of the voxels in struct image
-%   .Vs : handle to structural image
-%   .pf : pointer/handle to figure to use
-%   .pa : pointer/handle to axis object -> update the plot only!
-% 	.pl : pointer/handle to light object
-%   .p1/p2/p3 : pointer/handle to surface objects
-% 
-% OUPUT
-% - p_handles = handles to elements of the plot
-%   .p1/p2/p3 : handles to the structural isosurface, blob isosurface and
-%               structural cut surface resp.
-%   .pl : handles to the lights
-%   .pf : handle to the figure
-%   .pa : handle to the axis
-% - dat_out = data loaded/created in the execution
-%   .Ys : structural image data matrix
-%   .xyz_mm : structural image voxel coordinates (in mm)
-%   .Vs : mapped structral volume
-%   .Yl : blob/lesion image data matrix
-%   .range_mm : axis ranges in mm
+%   .p_fig : figure number (Def. create a new figure)
+%   .fig_pos : figure pisition/size (Def. ~a quarter of the screen)
+%
+% OUPUT 
+% - param_out : the same as the input parameters with some extra fields
+%   .range_mm : figure range (in mm) along the x/y/z axis
+%   .Vs : structure array containing structural image volume information
+%   .p_fig : figure number
+%   .fig_pos : figure position
+%   .handles : structure with the following sub-fields
+%       .p1/p2/p3 : pointer/handle to surface objects
+%       .pl : pointer/handle to light object
+%       .pa : pointer/handle to axis object
+%
+% or
+% INPUT for plot update
+% - param = the output parameter from anew plot, with updated
+%       + cutting parameters (axis, slice and direction)
+%       + poin of view (.pov field)
+%       as needed for the updated plot.
 %__________________________________________________________________
 % Copyright (C) 2015 Cyclotron Research Centre
 
 % Written by C. Phillips, 2015.
 % Cyclotron Research Centre, University of Liege, Belgium
+
+
+%% New plot or plot update?
+if nargin>1
+    new_plot = true;
+else
+    new_plot = false;
+end
 
 %% Check parameters
 param_def = struct(...
@@ -58,58 +66,103 @@ param_def = struct(...
     'col_blobs', 'green', ... % Colour for blobs
     'rotate3D',false ... % allow manual 3D rotation
     );
-if nargin>2
-    param = crc_check_flag(param_def,param);
-else
-    param = param_def;
-end
 
-if nargin<4
-    other_in = struct([]);
+if new_plot
+    if nargin==3
+        param = crc_check_flag(param_def,varargin{3});
+    else
+        param = param_def;
+    end
+else
+    param = varargin{1};
 end
 
 %% Check image input & load images
-if ischar(Ps)
-    % then load things
-    Vs = spm_vol(Ps);
-    [Ys,xyz_mm] = spm_read_vols(Vs);
+if new_plot
+    Ps = varargin{1};
+    Pl = varargin{2};
+    if ischar(Ps)
+        % then load things
+        Vs = spm_vol(Ps);
+        [Ys,xyz_mm] = spm_read_vols(Vs);
+    else
+        % Assume we're passing the values directly
+        % and pick coordinates from 'other_in
+        Ys = Ps;
+        xyz_mm = other_in.xyz_mm;
+        Vs = other_in.Vs;
+    end
+    
+    if ischar(Pl)
+        Vl = spm_vol(Pl);
+        Yl = spm_read_vols(Vl);
+    else
+        % Assume we're passing the values directly
+        Yl = Pl;
+    end
+    % Image sizes
+    sz_vx = size(Ys); % size in voxels
+    range_mm = [ ...
+        floor([min(xyz_mm(1,:)) min(xyz_mm(2,:)) min(xyz_mm(3,:))]) ; ...
+        ceil([max(xyz_mm(1,:)) max(xyz_mm(2,:)) max(xyz_mm(3,:))])];
+    % image axis in mm
+    vx_size = sqrt(sum(Vs.mat(:,1:3).^2)); % voxel size (in mm) for isosurf
+    x_mm = reshape(xyz_mm(1,:),sz_vx);
+    y_mm = reshape(xyz_mm(2,:),sz_vx);
+    z_mm = reshape(xyz_mm(3,:),sz_vx);
+    
+    % Set Figure
+    if isfield(param,'p_fig')
+        figure(param.p_fig)
+    else
+        param.p_fig = figure;
+    end
+    if ~isfield(param,'fig_pos')
+        scrsz = get(0,'ScreenSize');
+        param.fig_pos = [scrsz(3)/4 scrsz(4)/4 scrsz(3)/3 scrsz(4)/2];
+    end
+    set(param.p_fig,'Position',param.fig_pos) 
+
+    % Create & display patches for structural surface
+    FVs = isosurface(x_mm, y_mm, z_mm, Ys, param.int_thr);
+    vert = FVs.vertices;
+    Nvert = size(vert,1);
+    cdat = ones(size(FVs.vertices,1),1)*param.col_isosurf;
+    p1 =  patch(FVs, 'FaceColor', 'flat', 'FaceVertexCData', cdat, ...
+                'EdgeColor', 'none');
+    axis tight;
+    daspect(vx_size)
+    axis(range_mm(:)')
+    axis vis3d
+    axis off
+    xlabel('X-axis'); ylabel('Y-axis'); zlabel('Z-axis');
+    colormap(gray(100))
+    hold on
+    % and lesion surface 
+    FVl = isosurface(x_mm,y_mm,z_mm,Yl, .5);
+    p2 = patch(FVl, ...
+        'FaceColor', param.col_blobs, 'EdgeColor', 'none');
 else
-    % Assume we're passing the values directly
-    % and pick coordinates from 'other_in
-    Ys = Ps;
-    xyz_mm = other_in.xyz_mm;
-    Vs = other_in.Vs;
+    % Collect the necessary bits!
+    p1 = param.handles.p1;
+    vert = get(p1,'Vertices');
+    Nvert = size(vert,1);
+    p2 = param.handles.p2;
+    p3 = param.handles.p3;
+    range_mm = param.range_mm;
+    Vs = param.Vs;
+    vx_size = sqrt(sum(Vs.mat(:,1:3).^2)); % voxel size (in mm)
 end
 
-if ischar(Pl)
-    Vl = spm_vol(Pl);
-    Yl = spm_read_vols(Vl);
-else
-    % Assume we're passing the values directly
-    Yl = Pl;
-end
-
-% Image sizes
-sz_vx = size(Ys); % size in voxels
-range_mm = [ ...
-    floor([min(xyz_mm(1,:)) min(xyz_mm(2,:)) min(xyz_mm(3,:))]) ; ...
-    ceil([max(xyz_mm(1,:)) max(xyz_mm(2,:)) max(xyz_mm(3,:))])];
-% image axis in mm
-vx_size = sqrt(sum(Vs.mat(:,1:3).^2)); % voxel size (in mm) for isosurface
-Ys_surf = Ys;
-x_mm = reshape(xyz_mm(1,:),sz_vx);
-y_mm = reshape(xyz_mm(2,:),sz_vx);
-z_mm = reshape(xyz_mm(3,:),sz_vx);
-
-%% Deal with cut position and orientation
+%% Define surface cut and slice image
 switch param.cut_ax
     case 'x'
         % external isosurface
         if param.cut_dr>0
-            Ys_surf(xyz_mm(1,:)>param.cut_sl) = NaN;
+            lTransp = find(vert(:,1)>param.cut_sl);
             l_angle = [90 0];
         else
-            Ys_surf(xyz_mm(1,:)<param.cut_sl) = NaN;
+            lTransp = find(vert(:,1)<param.cut_sl);
             l_angle = [-90 0];
         end
         % cutting plane -> cap image
@@ -124,10 +177,10 @@ switch param.cut_ax
     case 'y'
         % external isosurface
         if param.cut_dr>0
-            Ys_surf(xyz_mm(2,:)>param.cut_sl) = NaN;
+            lTransp = find(vert(:,2)>param.cut_sl);
             l_angle = [180 0];
         else
-            Ys_surf(xyz_mm(2,:)<param.cut_sl) = NaN;
+            lTransp = find(vert(:,2)<param.cut_sl);
             l_angle = [0 0];
         end
         % cutting plane -> cap image
@@ -142,10 +195,10 @@ switch param.cut_ax
     case 'z'
         % external isosurface
         if param.cut_dr>0
-            Ys_surf(xyz_mm(3,:)>param.cut_sl) = NaN;
+            lTransp = find(vert(:,3)>param.cut_sl);
             l_angle = [0 90];
         else
-            Ys_surf(xyz_mm(3,:)<param.cut_sl) = NaN;
+            lTransp = find(vert(:,3)<param.cut_sl);
             l_angle = [0 -90];
         end
         % cutting plane -> cap image
@@ -166,78 +219,41 @@ cap_img = spm_sample_vol(Vs,sXYZ_vx(1,:)',sXYZ_vx(2,:)',sXYZ_vx(3,:)',1);
 cap_img = reshape(cap_img,size(sX));
 cap_img(cap_img<param.int_thr) = NaN;
 
-%% Dispaly
-if isfield(other_in,'pf')
-    figure(other_in.pf)
-else
-    scrsz = get(0,'ScreenSize');
-    figure('Position',[scrsz(3)/4 scrsz(4)/4 scrsz(3)/3 scrsz(4)/2])
-end
-if isfield(other_in,'pa')
-    update_plot = true;
-    %     delete(other_in.pa)
-else
-    update_plot = false;
-end
-if param.rotate3D
-    rotate3d on
-end
-
-% Isosurface for structure
-if ~update_plot
-    p1 = patch(isosurface(x_mm,y_mm,z_mm,Ys_surf, param.int_thr), ...
-        'FaceColor', param.col_isosurf, 'EdgeColor', 'none');
-    axis tight;
-    daspect(vx_size)
-    axis(range_mm(:)')
-    axis vis3d
-    axis off
-    xlabel('X-axis'); ylabel('Y-axis'); zlabel('Z-axis');
-    colormap(gray(100))
-    hold on
-else
-    % Update struct patch
-    PV = isosurface(x_mm,y_mm,z_mm,Ys_surf, param.int_thr);
-    set(other_in.p1,'Faces',PV.faces,'Vertices',PV.vertices)
-end
-
+%% Complete plot: add slice + transparent surface
+figure(param.p_fig)
+% Make transparent the right bit
+AlphaData = ones(Nvert,1); AlphaData(lTransp) = 0;
+set(p1,'FaceVertexAlphaData',AlphaData, 'FaceAlpha', 'interp');
 view(param.pov);
-
-if ~update_plot 
-    % Isosurface for the lesion
-    p2 = patch(isosurface(x_mm,y_mm,z_mm,Yl, .5), ...
-        'FaceColor', param.col_blobs, 'EdgeColor', 'none');
-end
-
-if ~update_plot
+% Deal with cut surface
+if new_plot
     % Cut surface  = volume cap
-    p3 = surface(sX,sY,sZ,cap_img,'LineStyle','none');
+    p3 = surface(sX, sY, sZ, cap_img, 'LineStyle', 'none');
     hold off
 else
     % update cut slice
-    set(other_in.p3,'XData',sX,'YData',sY,'ZData',sZ, ...
-        'CData',cap_img);
+    set(p3, 'XData', sX, 'YData', sY, 'ZData', sZ, 'CData', cap_img);
 end
 
-if ~update_plot
+%% Deal with lighting
+if new_plot
     % Add lights
     pl(1) = lightangle(param.pov(1),param.pov(2));
     pl(2) = lightangle(l_angle(1),l_angle(2));
     lighting gouraud
 else
-    % Update lights' position
+    % update lights
+    pl = param.handles.pl;
     [lX,lY,lZ] = sph2cart(param.pov(1),param.pov(2),2e3);
-    set(other_in.pl(1),'Position',[lX,lY,lZ]);
+    set(pl(1),'Position',[lX,lY,lZ]);
 end
 
 %% Output, only if not updating plot.
-if nargout>0 && ~update_plot
-    p_handles = struct('p1',p1,'p2',p2,'p3',p3,'pl',pl,'pf',gcf,'pa',gca);
-end
-if nargout>1 && ~update_plot
-    % Collect 3D images values + coordinates
-    dat_out = struct('Ys',Ys,'xyz_mm',xyz_mm,'Vs',Vs,'Yl',Yl, ...
-        'range_mm',range_mm);
+if nargout>0 && new_plot
+    param_out = param;
+    param_out.range_mm = range_mm;
+    param_out.Vs = Vs;
+    param_out.handles = struct('p1',p1,'p2',p2,'p3',p3,'pl',pl,'pa',gca);
 end
 
 end
@@ -259,16 +275,3 @@ end
 % colormap(gray(100))
 % camlight; lighting gouraud
 % isonormals(D, p);
-
-
-%% BROLL
-
-% pl2 = lightangle(l_angle(1),l_angle(2));
-% delete(pl2)
-% 
-% delete(other_in.pl(2))
-% other_in.pl(2) = lightangle(l_angle(1),l_angle(2))
-% 
-% pp = get(other_in.pl(1),'Position')
-% [a,e,r] = cart2sph(pp(1),pp(2),pp(3));
-% [aa,ee] = [a,e]./pi.*180
--- crc_disp_results.m
+++ crc_disp_results.m
@@ -25,10 +25,10 @@ cam_rotation = true;
 % Keep on rotating to return to original view
 do2rounds = true;
 % Filename of video created
-fn_vid = 'disp3Db.avi';
+fn_vid = 'disp3D.avi';
 
 %% Define Z-cut (i.e. horizontal cut) video
-z_step = 10 ; % steps for slices (in mm)
+z_step = 1 ; % steps for slices (in mm)
 z_list = -75:z_step:75;
 nFrames = numel(z_list);
 
@@ -39,19 +39,7 @@ param = struct(...
     'cut_dr', 1, ... % 1 (resp. -1) removing things >cut_sl (resp. <cut_sl)
     'int_thr', 10e-3 ... % Intensity threshold for surface/caps display
     );
-[p_handles, dat_out] = crc_disp_3Dlesion(Ps,Pl,param);
-
-% Collect infos for loop plot
-other_in = struct( ...
-    'xyz_mm', dat_out.xyz_mm, ...
-    'Vs', dat_out.Vs, ...
-    'pf', p_handles.pf, ...
-    'pa', p_handles.pa, ...
-    'pl', p_handles.pl, ...
-    'p1', p_handles.p1, ...
-    'p2', p_handles.p2, ...
-    'p3', p_handles.p3 ...
-    );
+[param_out] = crc_disp_3Dlesion(Ps,Pl,param);
 % Define rotating point-of-view (pov)
 if cam_rotation
     param.pov = [-120 sign(param.cut_dr)*30];
@@ -66,11 +54,12 @@ end
 
 % Prepare video
 vidObj = VideoWriter(fn_vid);
-vidObj.FrameRate = 5;
+vidObj.FrameRate = 20;
 vidObj.Quality = 95;
 open(vidObj);
 
 % Create frames and save them
+param = param_out;
 for ii = 1:nFrames
     param.cut_sl = z_list(ii);
     if cam_rotation,
@@ -78,9 +67,9 @@ for ii = 1:nFrames
     end
     % Update figure
     fprintf('\n Slice %c = %d', param.cut_ax, param.cut_sl);
-    crc_disp_3Dlesion(dat_out.Ys, dat_out.Yl, param, other_in);
+    crc_disp_3Dlesion(param);
     % Write each frame to the file.
-    currFrame = getframe(p_handles.pf);
+    currFrame = getframe(param.p_fig);
     writeVideo(vidObj,currFrame);
 end
 fprintf('\n')
