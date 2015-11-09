2764a8e9ee618aeeee944cfff7fd2d3b7cd746db
Christophe Phillips <c.phillips@ulg.ac.be>
2015-11-09 16:52:07 +0100

    Adding options into the batch configuration file
    Still need to use these new options in the crc_USwL exec function!

--- tbx_scfg_USwL.m
+++ tbx_scfg_USwL.m
@@ -67,6 +67,25 @@ imgOth.num     = [0 Inf];
 imgOth.val       = {''};
 
 % ---------------------------------------------------------------------
+% imgTpm TPM images
+% ---------------------------------------------------------------------
+imgTpm         = cfg_files;
+imgTpm.tag     = 'imgTpm';
+imgTpm.name    = 'Tissue probability maps';
+imgTpm.help    = {'Select the TPM images. '
+    ['A TPM_les file will be created with lesions as', ...
+    ' a new tissue class in 3rd position!']};
+imgTpm.filter  = 'image';
+imgTpm.ufilter = '.*';
+imgTpm.num     = [1 1];
+tmp = fullfile(spm('dir'),'tpm','unwTPM_sl2.nii');
+if exist(tmp,'file')
+    imgTpm.val{1}  = {tmp}; % Allow the CRC (and MPM) specific tpm's.
+else
+    imgTpm.val{1}  = {fullfile(spm('dir'),'tpm','TPM.nii')}; % or use standard SPM tpm's
+end
+
+% ---------------------------------------------------------------------
 % img4US Images to use for the segmentation
 % ---------------------------------------------------------------------
 img4US         = cfg_menu;
@@ -98,25 +117,6 @@ tpm4lesion.labels = {
 tpm4lesion.values = {0 1 2 3};
 tpm4lesion.val    = {1};
 
-% ---------------------------------------------------------------------
-% imgTpm TPM images
-% ---------------------------------------------------------------------
-imgTpm         = cfg_files;
-imgTpm.tag     = 'imgTpm';
-imgTpm.name    = 'Tissue probability maps';
-imgTpm.help    = {'Select the TPM images. '
-    ' A TPM_les file will be created with lesions as'
-    ' a new tissue class in position 3'};
-imgTpm.filter  = 'image';
-imgTpm.ufilter = '.*';
-imgTpm.num     = [1 1];
-tmp = fullfile(spm('dir'),'tpm','unwTPM_sl2.nii');
-if exist(tmp,'file')
-    imgTpm.val{1}  = {tmp}; % Allow the CRC (and MPM) specific tpm's.
-else
-    imgTpm.val{1}  = {fullfile(spm('dir'),'tpm','TPM.nii')}; % or use standard SPM tpm's
-end
-
 %--------------------------------------------------------------------------
 % biasreg Bias regularisation
 %--------------------------------------------------------------------------
@@ -124,44 +124,111 @@ biasreg         = cfg_menu;
 biasreg.tag     = 'biasreg';
 biasreg.name    = 'Bias regularisation';
 biasreg.help    = {
-                   'MR images are usually corrupted by a smooth, spatially varying artifact that modulates the intensity of the image (bias). These artifacts, although not usually a problem for visual inspection, can impede automated processing of the images.'
-                   ''
-                   'An important issue relates to the distinction between intensity variations that arise because of bias artifact due to the physics of MR scanning, and those that arise due to different tissue properties.  The objective is to model the latter by different tissue classes, while modelling the former with a bias field. We know a priori that intensity variations due to MR physics tend to be spatially smooth, whereas those due to different tissue types tend to contain more high frequency information. A more accurate estimate of a bias field can be obtained by including prior knowledge about the distribution of the fields likely to be encountered by the correction algorithm. For example, if it is known that there is little or no intensity non-uniformity, then it would be wise to penalise large values for the intensity non-uniformity parameters. This regularisation can be placed within a Bayesian context, whereby the penalty incurred is the negative logarithm of a prior probability for any particular pattern of non-uniformity.'
-                   'Knowing what works best should be a matter of empirical exploration.  For example, if your data has very little intensity non-uniformity artifact, then the bias regularisation should be increased.  This effectively tells the algorithm that there is very little bias in your data, so it does not try to model it.'
-                   }';
+    ['MR images are usually corrupted by a smooth, spatially varying ', ...
+    'artifact that modulates the intensity of the image (bias). These ', ...
+    'artifacts, although not usually a problem for visual inspection, ', ...
+    'can impede automated processing of the images.']
+    ''
+    ['An important issue relates to the distinction between intensity ', ...
+    'variations that arise because of bias artifact due to the physics ', ...
+    'of MR scanning, and those that arise due to different tissue ', ...
+    'properties.  The objective is to model the latter by different ', ...
+    'tissue classes, while modelling the former with a bias field. We ', ...
+    'know a priori that intensity variations due to MR physics tend to ', ...
+    'be spatially smooth, whereas those due to different tissue types ', ...
+    'tend to contain more high frequency information. A more accurate ', ...
+    'estimate of a bias field can be obtained by including prior ', ...
+    'knowledge about the distribution of the fields likely to be ', ...
+    'encountered by the correction algorithm. For example, if it is ', ...
+    'known that there is little or no intensity non-uniformity, then it ', ...
+    'would be wise to penalise large values for the intensity ', ...
+    'non-uniformity parameters. This regularisation can be placed within ', ...
+    'a Bayesian context, whereby the penalty incurred is the negative ', ...
+    'logarithm of a prior probability for any particular pattern of non-uniformity.']
+    'Knowing what works best should be a matter of empirical exploration.  For example, if your data has very little intensity non-uniformity artifact, then the bias regularisation should be increased.  This effectively tells the algorithm that there is very little bias in your data, so it does not try to model it.'
+    }';
 biasreg.labels = {
-                  'no regularisation (0)'
-                  'extremely light regularisation (0.00001)'
-                  'very light regularisation (0.0001)'
-                  'light regularisation (0.001)'
-                  'medium regularisation (0.01)'
-                  'heavy regularisation (0.1)'
-                  'very heavy regularisation (1)'
-                  'extremely heavy regularisation (10)'
-                  }';
+    'no regularisation (0)'
+    'extremely light regularisation (0.00001)'
+    'very light regularisation (0.0001)'
+    'light regularisation (0.001)'
+    'medium regularisation (0.01)'
+    'heavy regularisation (0.1)'
+    'very heavy regularisation (1)'
+    'extremely heavy regularisation (10)'
+    }';
 biasreg.values = {
-                  0
-                  1e-05
-                  0.0001
-                  0.001
-                  0.01
-                  0.1
-                  1
-                  10
-                  }';
-biasreg.val    = {1e-05}; % the default is almost nothing, assuming we use MPMs
+    0
+    1e-05
+    0.0001
+    0.001
+    0.01
+    0.1
+    1
+    10
+    }';
+biasreg.val = {1e-05}; % the default is almost nothing, assuming we use MPMs
 
+%--------------------------------------------------------------------------
+% biasfwhm Bias FWHM
+%--------------------------------------------------------------------------
+biasfwhm         = cfg_menu;
+biasfwhm.tag     = 'biasfwhm';
+biasfwhm.name    = 'Bias FWHM';
+biasfwhm.help    = {'FWHM of Gaussian smoothness of bias. If your ', ...
+    'intensity non-uniformity is very smooth, then choose a large FWHM. ', ...
+    'This will prevent the algorithm from trying to model out intensity ', ...
+    'variation due to different tissue types. The model for intensity ', ...
+    'non-uniformity is one of i.i.d. Gaussian noise that has been ', ...
+    'smoothed by some amount, before taking the exponential. Note also ', ...
+    'that smoother bias fields need fewer parameters to describe them. ', ...
+    'This means that the algorithm is faster for smoother intensity non-uniformities.'};
+biasfwhm.labels = {
+    '30mm cutoff'
+    '40mm cutoff'
+    '50mm cutoff'
+    '60mm cutoff'
+    '70mm cutoff'
+    '80mm cutoff'
+    '90mm cutoff'
+    '100mm cutoff'
+    '110mm cutoff'
+    '120mm cutoff'
+    '130mm cutoff'
+    '140mm cutoff'
+    '150mm cutoff'
+    'No correction'
+    }';
+biasfwhm.values = {
+    30
+    40
+    50
+    60
+    70
+    80
+    90
+    100
+    110
+    120
+    130
+    140
+    150
+    Inf
+    }';
+biasfwhm.val    = {Inf}; % No bias correction
 
 % ---------------------------------------------------------------------
-% Number of Gaussians used to model the lesion
+% Number of Gaussians per tissue class used to model the lesion
 % ---------------------------------------------------------------------
 NbGaussian         = cfg_entry;
 NbGaussian.tag     = 'NbGaussian';
-NbGaussian.name    = 'Number of Gaussians';
-NbGaussian.help    = {'Set the number of Gaussians to model the lesion'};
+NbGaussian.name    = 'Number of Gaussians per tissue class';
+NbGaussian.help    = {'Set the number of Gaussians per tissue class to model the intensity histograms.' ,...
+    ['The 7 numbers corresponds to the 7 tissue classess in the following order : ', ...
+    'GM, WM, Lesion, CSF, Skull, Soft tissues, and Air']};
 NbGaussian.strtype = 'n';
-NbGaussian.num     = [1 1];
-NbGaussian.val     = {2}; 
+NbGaussian.num     = [1 7];
+NbGaussian.val     = {[2 2 2 2 3 4 2]};
 
 % ---------------------------------------------------------------------
 % thrMPM Threshold outlier values from the MPM
@@ -170,7 +237,7 @@ thrMPM         = cfg_menu;
 thrMPM.tag     = 'thrMPM';
 thrMPM.name    = 'Thresholding the MPMs';
 thrMPM.help    = {['Apply a threshold on the MPM''s to remove outlier ',...
-    'values from the images.'],...
+    'values from the images before the segmentation itself.'],...
     ['Take the absolute value of voxels < 0. Voxels > thr are set to thr ',...
     '+ small random number. The ''thr'' value is defined for each ',...
     'MPM (A/MT/R1/R2) seperately.'],...
@@ -184,6 +251,25 @@ thrMPM.values = {0 1};
 thrMPM.val    = {1};
 
 % ---------------------------------------------------------------------
+% ICVmsk Create ICV-mask and mask the MPMs
+% ---------------------------------------------------------------------
+ICVmsk         = cfg_menu;
+ICVmsk.tag     = 'ICVmsk';
+ICVmsk.name    = 'Create ICV-mask and mask the MPMs';
+ICVmsk.help    = {['An ICV mask can be created from the reference structural ', ...
+    'image and applied onto the MPMs before the segmentation itself. ',...
+    'This cleans up the images quite a bit and ', ...
+    'is equivalent to "skull stripping". This helps, in some cases, the ', ...
+    'multi-channel segmentation of the MPMs.'], ...
+    'The masked MPM images are prefixed with ''k''.'};
+ICVmsk.labels = {
+    'No'
+    'Yes'
+    }';
+ICVmsk.values = {0 1};
+ICVmsk.val    = {1};
+
+% ---------------------------------------------------------------------
 % thrLesion Threshold lesion mask based on spatial extent
 % ---------------------------------------------------------------------
 thrLesion         = cfg_entry;
@@ -194,22 +280,35 @@ thrLesion.help    = {'Apply a spatial threshold on the lesion tissue class c3. '
 thrLesion.num    = [1 1];
 thrLesion.val    = {0};
 
-% ---------------------------------------------------------------------
-% ICVmsk Create ICV-mask and mask the MPMs
-% ---------------------------------------------------------------------
-ICVmsk         = cfg_menu;
-ICVmsk.tag     = 'ICVmsk';
-ICVmsk.name    = 'Create ICV-mask and mask the MPMs';
-ICVmsk.help    = {'An ICV mask can be created from the reference structural '
-    'image and applied onto the MPMs. This cleans up the images quite a bit and'
-    'is equivalent to "skull stripping". This helps, in some cases, the '
-    'multi-channel segmentation of the MPMs.'};
-ICVmsk.labels = {
-    'No'
-    'Yes'
+%--------------------------------------------------------------------------
+% mrf MRF Parameter
+%--------------------------------------------------------------------------
+mrf         = cfg_entry;
+mrf.tag     = 'mrf';
+mrf.name    = 'MRF Parameter';
+mrf.help    = {'When tissue class images are written out, a few iterations of a simple Markov Random Field (MRF) cleanup procedure are run.  This parameter controls the strength of the MRF. Setting the value to zero will disable the cleanup.'};
+mrf.strtype = 'r';
+mrf.num     = [1 1];
+mrf.val     = {1};
+
+%--------------------------------------------------------------------------
+% cleanup Clean up any partitions
+%--------------------------------------------------------------------------
+cleanup         = cfg_menu;
+cleanup.tag     = 'cleanup';
+cleanup.name    = 'Clean Up';
+cleanup.help    = {
+    'This uses a crude routine for extracting the brain from segmented images.  It begins by taking the white matter, and eroding it a couple of times to get rid of any odd voxels.  The algorithm continues on to do conditional dilations for several iterations, where the condition is based upon gray or white matter being present.This identified region is then used to clean up the grey and white matter partitions.  Note that the fluid class will also be cleaned, such that aqueous and vitreous humour in the eyeballs, as well as other assorted fluid regions (except CSF) will be removed.'
+    ''
+    'If you find pieces of brain being chopped out in your data, then you may wish to disable or tone down the cleanup procedure. Note that the procedure uses a number of assumptions about what each tissue class refers to.  If a different set of tissue priors are used, then this routine should be disabled.'
     }';
-ICVmsk.values = {0 1};
-ICVmsk.val    = {1};
+cleanup.labels = {
+    'Dont do cleanup'
+    'Light Clean'
+    'Thorough Clean'
+    }';
+cleanup.values = {0 1 2};
+cleanup.val    = {1};
 
 % ---------------------------------------------------------------------
 % options Options
@@ -217,7 +316,7 @@ ICVmsk.val    = {1};
 options         = cfg_branch;
 options.tag     = 'options';
 options.name    = 'Options';
-options.val     = {imgTpm img4US biasreg NbGaussian tpm4lesion thrMPM thrLesion ICVmsk};  
+options.val     = {imgTpm img4US biasreg biasfwhm NbGaussian tpm4lesion thrMPM ICVmsk mrf cleanup thrLesion};
 options.help    = {'Some processing options.'};
 %_______________________________________________________________________
 
@@ -268,7 +367,7 @@ if job.options.thrMPM
     end
     for ii=1:numel(job.imgMPM)
         cdep(end+1)          = cfg_dep; %#ok<*AGROW>
-%         cdep(end).sname      = sprintf('Mask thresholded MPM #%d',ii);
+        %         cdep(end).sname      = sprintf('Mask thresholded MPM #%d',ii);
         cdep(end).sname      = sprintf('Threshold-mask MPM #%d',ii);
         cdep(end).src_output = substruct('.',sprintf('thrMPMmsk%d',ii));
         cdep(end).tgt_spec   = cfg_findspec({{'filter','nifti'}});
