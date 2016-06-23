40c7b4ed2e6e179d2e2d873ae32d99283569d235
Christophe Phillips <c.phillips@ulg.ac.be>
2016-06-23 14:31:50 +0200

    Provide the created filename as output + chose prefix
    - now returning the generated file name
    - let you also chose the prefix you want

--- crc_lesion_cleanup.m
+++ crc_lesion_cleanup.m
@@ -1,32 +1,41 @@
-function crc_lesion_cleanup(P,k)
+function Pout = crc_lesion_cleanup(Pin,k,prefix)
 
 % this is a silly routine to cleanup the lesion mask (c3) based on spatial
 % extend, during segmentation some voxels may endup misclassified and if
 % we know we have lesions of a given size, we can use spatial extend to
 % cleanup
 %
-% FORMAT lesion_crc_cleanup(P,k)
+% FORMAT 
+% Pout = lesion_crc_cleanup(Pin,k)
 %
-% INPUT P is the volume name (c3 image)
-%        k is the spatial extent threshold
+% INPUT 
+% Pin    : the volume name (typically c3 image, the lesion map)
+% k      : the spatial extent threshold [Inf, def., i.e largest cluster]
+% prefix : prefix prepended to image name
 %
-% OUTPUT clean_P is writen on the disk
+% OUTPUT 
+% Pout   : output file name of cleaned image
+% clean_P is writen on the disk
+% 
 %
 % see also spm_bwlabel
 % Cyril Pernet 30 Oct 2015
 %_______________________________________________________________________
 % Copyright (C) 2015 Cyclotron Research Centre
 
+if narg<2
+    prefix = 'cleaned_';
+end
+if nargin<1
+    k = Inf; % if not specified we keep only the biggest cluster
+end
 if nargin == 0
-    P = spm_select(1,'image','select image to threshold');
-    k = Inf; % if not specified we keep only the biggest cluster
-elseif nargin == 1
-    k = Inf; % if not specified we keep only the biggest cluster
+    Pin = spm_select(1,'image','select image to threshold');
 end
 
 % get clusters
-if iscell(P); P=cell2mat(P); end    
-V = spm_vol(P);
+if iscell(Pin); Pin = cell2mat(Pin); end    
+V = spm_vol(Pin);
 data = spm_read_vols(V);
 [clustered_map,num] = spm_bwlabel(double(data>0),6); % note we use surface connection only
 
@@ -37,7 +46,7 @@ nv = histc(clustered_map,0:num);
 [~,idxall]=sort(clustered_map,'ascend');
 idxall(1:nv(1)) = []; nv(1)=[]; % remove 1st bin ie 0s
 ends = cumsum(nv);
-inis=ends-nv+1;
+inis = ends-nv+1;
 
 % make the new map - if cluster >=k then = 1
 if isinf(k) || ischar(k);
@@ -48,7 +57,7 @@ if isinf(k) || ischar(k);
 else
     for i=1:num
         idx=idxall(inis(i):ends(i));
-        if nv(i) >=k
+        if nv(i) >= k
             extent_map(idx)=1;
         else
             extent_map(idx)=0;
@@ -57,8 +66,8 @@ else
 end
 
 extent_map = extent_map .* data; % reasign the initial values
-[root,name,ext]=spm_fileparts(V.fname);
-V.fname = [root filesep 'cleaned_' name ext];
+Pout = spm_file(V.fname,'prefix',prefix);
+V.fname = Pout;
 V.descrip = [V.descrip ' thresholded @ k=' num2str(k)];
 V.private.descrip = V.descrip;
 spm_write_vol(V,extent_map);
