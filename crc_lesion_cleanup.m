b075b3213ed55f1cf2ba096336a035ce1f188fef
Christophe Phillips <c.phillips@ulg.ac.be>
2016-07-20 17:39:20 +0200

    Small improvements
    - use standard naming
    - now can work directly with passed array
    - returning the number of dropped/remaining clusters

--- crc_lesion_cleanup.m
+++ crc_lesion_cleanup.m
@@ -1,21 +1,22 @@
-function Pout = crc_lesion_cleanup(Pin,k,prefix)
+function [fn_out,nDropped] = crc_lesion_cleanup(fn_in,k,prefix)
 
-% this is a silly routine to cleanup the lesion mask (c3) based on spatial
-% extend, during segmentation some voxels may endup misclassified and if
-% we know we have lesions of a given size, we can use spatial extend to
-% cleanup
+% This is a silly routine to cleanup anu binary image based on spatial
+% extent of clsuters. During segmentation some spurious voxels may end up 
+% misclassified. So if we know a clsuter (e.g. lesions) must be at least of
+% a given size, we can use spatial extend to clean up the small clusters.
 %
 % FORMAT 
-% Pout = lesion_crc_cleanup(Pin,k)
+% fn_out = lesion_crc_cleanup(fn_in,k,prefix)
 %
 % INPUT 
-% Pin    : the volume name (typically c3 image, the lesion map)
-% k      : the spatial extent threshold [Inf, def., i.e largest cluster]
-% prefix : prefix prepended to image name
+% fn_in     : the volume name (typically c3 image, the lesion map)
+% k         : the spatial extent threshold [Inf, def., i.e largest cluster]
+% prefix    : prefix prepended to image name
 %
 % OUTPUT 
-% Pout   : output file name of cleaned image
-% clean_P is writen on the disk
+% fn_out    : file name of cleaned image, writen on the disk
+% nDropped  : number of clusters that were dropped out and remaining number
+%             of clusters.
 % 
 %
 % see also spm_bwlabel
@@ -30,14 +31,22 @@ if nargin<1
     k = Inf; % if not specified we keep only the biggest cluster
 end
 if nargin == 0
-    Pin = spm_select(1,'image','select image to threshold');
+    fn_in = spm_select(1,'image','select image to threshold');
 end
 
-% get clusters
-if iscell(Pin); Pin = cell2mat(Pin); end    
-V = spm_vol(Pin);
-data = spm_read_vols(V);
-[clustered_map,num] = spm_bwlabel(double(data>0),6); % note we use surface connection only
+% get image in
+if iscell(fn_in); fn_in = cell2mat(fn_in); end
+if ischar(fn_in)
+    V_in = spm_vol(fn_in);
+    data = spm_read_vols(V_in);
+    save_img = true;
+else
+    data = fn_in;
+    save_img = false;
+end
+
+[clustered_map,num] = spm_bwlabel(double(data>0),6);
+% note we use surface connection only
 
 % check number of elements per clusters
 extent_map = zeros(size(data));
@@ -53,34 +62,31 @@ if isinf(k) || ischar(k);
     [~,pos] = max(nv);
     idx=idxall(inis(pos):ends(pos));
     extent_map(idx)=1;
-    
+    nDropped = [numel(nv)-1 1];
 else
+    nDropped = 0;
     for i=1:num
         idx=idxall(inis(i):ends(i));
         if nv(i) >= k
             extent_map(idx)=1;
         else
             extent_map(idx)=0;
+            nDropped = nDropped + 1;
         end
     end
+    nDropped(2) = num-nDropped(1);
 end
 
 extent_map = extent_map .* data; % reasign the initial values
-Pout = spm_file(V.fname,'prefix',prefix);
-V.fname = Pout;
-V.descrip = [V.descrip ' thresholded @ k=' num2str(k)];
-V.private.descrip = V.descrip;
-spm_write_vol(V,extent_map);
-fprintf('%s created \n',V.fname)
+if save_img
+    fn_out = spm_file(V_in.fname,'prefix',prefix);
+    V_out.fname = fn_out;
+    V_out.descrip = [V_out.descrip ' thresholded @ k=' num2str(k)];
+    V_out.private.descrip = V_out.descrip;
+    spm_write_vol(V_out,extent_map);
+    fprintf('%s created \n',V_out.fname)
+else
+    fn_out = extent_map;
+end
 
 end
-% to make an extent map do
-% for i=1:num
-%     idx=idxall(inis(i):ends(i));
-%     extent_map(idx)=nv(i);
-% end
-% V.fname = [root filesep 'extent_map_' name ext];
-% V.descrip = [V.descrip ' spatial extent map'];
-% V.private.descrip = V.descrip;
-% spm_write_vol(V,extent_map);
-% fprintf('%s created \n',V.fname)
