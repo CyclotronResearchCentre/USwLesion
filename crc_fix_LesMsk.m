0dbbb71efe08caf71ba991ac3597a0f569b672ba
Christophe Phillips <c.phillips@uliege.be>
2018-02-22 13:43:46 +0100

    adding 2nd part of lesion mask fix + error fixes

--- crc_fix_LesMsk.m
+++ crc_fix_LesMsk.m
@@ -40,6 +40,10 @@ function fn_tmsk = crc_fix_LesMsk(fn_msk,opt)
 % Written by C. Phillips.
 % Cyclotron Research Centre, University of Liege, Belgium
 
+%% Fixed parameters
+prct_threshold = 95; % percentile threshold
+use_mean_or_med = 1; % use mean (1) of all lesion voxel intensities or median (2)
+
 %% 0. Check input & Load things
 if nargin<2
     opt = struct;
@@ -50,7 +54,7 @@ opt_def = struct(...
 opt = crc_check_flag(opt_def,opt);
 
 Vmsk = spm_vol(fn_msk);
-[vMsk,XYZmm] = spm_read_vols(Vmsk);
+vMsk = spm_read_vols(Vmsk);
 
 % Ensures values are [0 1], in case scaling was wrong, e.g. [0 255], or
 % there are some tiny negative values, e.g. if mask was resampled
@@ -61,10 +65,10 @@ if max(vMsk(:))>1 || min(vMsk(:))<0
     vMsk(vMsk>=.5) = 1; % values >=.5, set to 1
     vMsk(vMsk<.5) = 0; % anything <.5 set to zero.
 end
-
+vMsk = round(vMsk);
 % VX coordinates
-XYZvx = round(Vmsk.mat\[XYZmm ; ones(1,size(XYZmm,2))]);
-XYZvx(4,:) = [];
+% XYZvx = round(Vmsk.mat\[XYZmm ; ones(1,size(XYZmm,2))]);
+% XYZvx(4,:) = [];
 
 % minVol mm3 -> vx
 vx_vol = abs(det(Vmsk.mat(1:3,1:3)));
@@ -83,29 +87,65 @@ end
 
 % clusters to remove
 l_clust2rem = find(N<minNrVx);
-
 % Create fixed volume w/o clust2rem
-tvMsk = zeros(numel(vMsk),1);
+tvMsk = zeros(size(vMsk));
 for ii=1:num
     if ~any(ii==l_clust2rem)
-        tvMsk(L(:)==ii) = 1;
+        tvMsk(L(:)==ii) = 1; % keep cluster
+    else
+        L(L(:)==ii) = 0; % remove from clusters
     end
 end
-tvMsk = reshape(tvMsk,size(vMsk));
 
 %% 2. removing clusters based on intensities in adjoining image
 if ~isempty(opt.fn_oth)
-    % Load up other image
+    % deal with other image
+    Voth = spm_vol(opt.fn_oth);
     % check is has same size as lesion mask!
-    % proceed
+    if all(Vmsk.dim == Voth.dim)
+        % proceed with remaining clusters
+        clust_list = 1:num; clust_list(l_clust2rem) = [];
+        vOth = spm_read_vols(Voth);
+        switch use_mean_or_med
+            case 1 % use lesion(s) global mean 
+                v_threshold = mean(vOth(~~tvMsk(:)));
+            case 2 % use lesion(s) global median
+                v_threshold = median(vOth(~~tvMsk(:)));
+        end
+        % load up values per cluster
+        n_clust_list = numel(clust_list);
+        v_les_fla_clust = cell(1,n_clust_list);
+        prct_les_fla_clust = zeros(1,n_clust_list);
+        for ii=1:n_clust_list
+            v_les_fla_clust{ii} = vOth(L(:)==clust_list(ii));
+            prct_les_fla_clust(ii) = prctile(v_les_fla_clust{ii},prct_threshold);
+        end
+        % actually it's not necessary to save cluster intensities but could
+        % be for other checks... so keep it just in case.
+        l_clust2rem = clust_list(prct_les_fla_clust<v_threshold);
+        % remove these ones
+        for ii=1:num
+            if ~any(ii==l_clust2rem)
+                tvMsk(L(:)==ii) = 1; % keep cluster
+            else
+                L(L(:)==ii) = 0; % remove from clusters
+            end
+        end
+    else
+        beep;
+        fprintf(['\nOther image size [%d %d %d] does not match that of '...
+            'the mask [%d %d %d].\nNot proceeding any further!\n'], ...
+            [Voth.dim Vmsk.dim])
+    end
 end
 
 %% Save image & return filename
+
 Vtmsk = Vmsk;
 Vtmsk.fname = spm_file(Vmsk.fname,'prefix','t');
 Vtmsk = spm_create_vol(Vtmsk);
 Vtmsk = spm_write_vol(Vtmsk,tvMsk); %#ok<*NASGU>
-
+% output filename
 fn_tmsk = Vtmsk.fname;
 
 end
